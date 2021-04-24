09ab3793952c05619e0ab9630e587ac672798ef3
Aleksandr Polonnikov <kansk.sasha@live.com>
2021-04-24 19:15:50 +0700

    Reading the frequency of characters and building a list

--- Haffman/Haffman.cpp
+++ Haffman/Haffman.cpp
@@ -1,20 +1,95 @@
-﻿// Haffman.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <cstddef>
+#include <list>
+#define NUM_OF_CHARS 256
 
-#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    char token=0;
+    int freq=0;
+    Node* left=nullptr, * rigth=nullptr;
+
+    Node()
+    {
+
+    }
+
+    Node(char _token, int _freq)
+    {
+        token = _token;
+        freq = _freq;
+    }
+
+    Node(Node* _left, Node* _rigth)
+    {
+        freq = _left->freq + _rigth->freq;
+        left = _left;
+        rigth = _rigth;
+    }
+
+    ~Node()
+    {
+
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+    int frequency[NUM_OF_CHARS] = { 0 };
+    char c=0;
+    while (1)
+    {
+        c = cin.get();
+        if (cin.eof())
+        {
+            break;
+        }
+        frequency[c]++;
+    }
+
+    list<Node>freq_list;
+
+    for (int i = 0; i < NUM_OF_CHARS; i++)
+    {
+        if (frequency[i]==0)
+        {
+            continue;
+        }
+
+        Node temp(i, frequency[i]);
+
+        auto iter = freq_list.begin();
+
+        for (; iter!=freq_list.end();)
+        {
+            if (temp.freq>iter->freq)
+            {
+                iter++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        freq_list.insert(iter, temp);
+    }
+
+
+    for (auto it : freq_list)
+    {
+        cout << it.token << " " << it.freq << endl;
+    }
+    /*
+    for (int i = 0; i < NUM_OF_CHARS; i++)
+    {
+        cout << frequency[i] << endl;
+    }
+    */
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
